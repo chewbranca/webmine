58bc30458c0eeb8891c35be0a3faaac56614ecdf
aria42 <me@aria42.com>
2011-05-09 15:22:25 -0700

    pruned project.clj

--- project.clj
+++ project.clj
@@ -4,8 +4,6 @@
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [xerces/xercesImpl "2.9.1"]
-                 [clj-sys/tagsoup "1.3-SNAPSHOT"]
-                 [clj-serializer "0.1.1"]
                  [html-parse "0.0.1-SNAPSHOT"]
                  [xalan "2.7.1"]
                  [rome "0.9"]
@@ -18,6 +16,5 @@
   :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]]
   :jvm-opts ["-server"  "-mx1800m" "-Djava.awt.headless=true" "-Dfile.encoding=UTF8"]
 
-  :repositories  {"apache" "https://repository.apache.org/content/repositories/releases/"
-                  "snapshots" "http://mvn.getwoven.com/repos/woven-public-snapshots"
-                  "releases" "http://mvn.getwoven.com/repos/woven-public-releases"})
+  :repositories  {"apache" "https://repository.apache.org/content/repositories/releases/"})
+
