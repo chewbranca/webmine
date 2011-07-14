0a51e4362e7fe1d189e22b278f8aa9cbaf138edd
Aria Haghighi <me@aria42.com>
2011-07-13 22:12:38 -0700

    updated work

--- project.clj
+++ project.clj
@@ -8,7 +8,7 @@
                  [xalan "2.7.1"]
                  [infer "1.0.2-SNAPSHOT"]
                  [fetcher "0.0.5-SNAPSHOT"]
-                 [work "1.1.2-SNAPSHOT"]
+                 [work "1.1.3-SNAPSHOT"]
                  [clj-time "0.4.0-SNAPSHOT"]
                  [clj-sys/plumbing "0.1.5-SNAPSHOT"]]
   :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]]
